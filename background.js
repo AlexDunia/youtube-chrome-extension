db3f971dfb26e003711f7c903ebad30ea3ca4b6f
Alex Dunia <thealexdunia@gmail.com>
2024-08-24 20:05:12 +0100

    first commit

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log("now working");
+  if (changeInfo.status === 'complete' && tab.url && tab.url.includes("youtube.com/watch")) {
+      const queryParameters = tab.url.split("?")[1];
+      const urlParameters = new URLSearchParams(queryParameters);
+      console.log(urlParameters);
+
+      chrome.tabs.sendMessage(tabId, {
+        type: "NEW",
+        videoId: urlParameters.get("v"),
+    }).catch(error => {
+        console.error("Message failed:", error);
+    });
+
+  }
+});
